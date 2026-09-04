10d16707ab76d243d964f88dbdb048125669e765
danova <danovacavalaria@gmail.com>
2026-09-04 19:39:53 -0300

    atualização do terraform

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -1,9 +1,9 @@
 module "eks" {
   source = "terraform-aws-modules/eks/aws"
 
-  cluster_name                    = var.cluster_name
-  cluster_version                 = "1.24"
-  cluster_endpoint_public_access  = true
+  name                    = var.cluster_name
+  kubernetes_version      = "1.24"
+  endpoint_public_access  = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.public_subnets
