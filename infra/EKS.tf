8894235b3844f84c2d96d112482879fb618d3a2d
danova <danovacavalaria@gmail.com>
2026-09-05 16:06:15 -0300

    corrigido arquivo EKS.tf

--- infra/EKS.tf
+++ infra/EKS.tf
@@ -20,7 +20,7 @@ module "eks" {
       max_size     = 10
       desired_size = 3
       vpc_security_group_ids = [aws_security_group.ssh_cluster.id]
-      instance_types = ["t2.micro"]
+      instance_types = ["t3.micro"]
     }
   }
 }
